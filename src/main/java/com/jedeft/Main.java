dc2c944b1e71f43106add7f1ee1f8b774d4b2275
Jedeft <jedeft@163.com>
2017-02-24 17:55:37 +0800

    add log4j

--- src/main/java/com/jedeft/Main.java
+++ src/main/java/com/jedeft/Main.java
@@ -1,6 +1,7 @@
 package com.jedeft;
 
 import com.jedeft.utils.FileUtil;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
@@ -9,18 +10,20 @@ import java.util.List;
  * Created by jedeft on 2017/2/24.
  */
 public class Main {
+    private static Logger logger = Logger.getLogger("management");
+
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Please input fileName params");
+            System.out.println("Please input filePath params !");
             System.exit(-1);
         }
         try {
-            List<String> lineList = FileUtil.loadFile("input.txt");
+            List<String> lineList = FileUtil.loadFile(args[0]);
             for (String line : lineList) {
                 System.out.println(line);
             }
         } catch (IOException e) {
-            System.out.println(e.toString());
+            logger.error(e);
             System.exit(-1);
         }
     }
